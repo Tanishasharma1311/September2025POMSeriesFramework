354c73028e36f40f9933c3de1105d0ef2bdab72d
tanisha <sharmatanisha138@gmail.com>
2025-09-25 12:39:29 +0530

    updated cart page as per Naveen TL review comments

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,8 +1,14 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
 	
-	String product = "macbook";
+By product = By.id("productId");
+
+public void clickProduct() {
+	System.out.println("click on macbook");
+}
 	
 
 }
